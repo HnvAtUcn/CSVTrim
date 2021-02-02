1e2d0216976dcc3cbb5920e4d9e15b1052033d71
HnvAtUcn <hnv@ucn.dk>
2021-02-02 13:02:31 +0100

    Version with control of hack

--- CSVTrim/Form1.Designer.cs
+++ CSVTrim/Form1.Designer.cs
@@ -28,13 +28,54 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            this.openFileDialog1 = new System.Windows.Forms.OpenFileDialog();
+            this.label1 = new System.Windows.Forms.Label();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.SuspendLayout();
+            // 
+            // openFileDialog1
+            // 
+            this.openFileDialog1.FileName = "openFileDialog1";
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Arial", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(12, 9);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(207, 15);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Drop name of folder with files to trim:";
+            // 
+            // textBox1
+            // 
+            this.textBox1.AllowDrop = true;
+            this.textBox1.Font = new System.Drawing.Font("Arial", 10F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Pixel, ((byte)(0)));
+            this.textBox1.Location = new System.Drawing.Point(222, 7);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(442, 19);
+            this.textBox1.TabIndex = 1;
+            this.textBox1.TextChanged += new System.EventHandler(this.textBox1_TextChanged);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
-            this.Text = "Form1";
+            this.ClientSize = new System.Drawing.Size(687, 35);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.label1);
+            this.Name = "Form1";
+            this.Text = "CSVTrim";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
         }
 
         #endregion
+
+        private System.Windows.Forms.OpenFileDialog openFileDialog1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox textBox1;
     }
 }
 
--- CSVTrim/Form1.cs
+++ CSVTrim/Form1.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
 using System.Drawing;
+using System.IO;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -12,9 +13,97 @@ namespace CSVTrim
 {
     public partial class Form1 : Form
     {
+        String folderpath;
+        String tail = ";;;;;;;;;;;;;;\n"; // A lot of empty (redundant) data fields in the output CSV file
+
         public Form1()
         {
             InitializeComponent();
+        }
+
+        private void textBox1_TextChanged(object sender, EventArgs e)
+        {
+            folderpath = textBox1.Text;
+
+            // If a valid foldername was entered in the TextBox, then...
+            if (Directory.Exists(folderpath))
+            {
+                // A subfolder named "Trimmed" is created.
+                string filedirectoryTrimmed = folderpath + "\\Trimmed";
+                Directory.CreateDirectory(filedirectoryTrimmed);
+
+                foreach (string filepath in Directory.EnumerateFiles(folderpath, "*.csv"))
+                {
+                    // For each file to be trimmed, a new file with its name + "Trimmed" is 
+                    // created in the subfolder. The output is written to that file
+                    string filenameTrimmed = Path.GetFileNameWithoutExtension(filepath);
+                    string filePathTrimmed = filedirectoryTrimmed + "\\" + filenameTrimmed + "Trimmed.csv";
+
+                    //Console.WriteLine("IN " + folderpath + ": " + filepath);
+                    string output = "";
+                    
+                    using (FileStream fs = new FileStream(filepath, FileMode.Open, FileAccess.Read))
+                    {
+                        using (StreamReader r = new StreamReader(fs))
+                        {
+                            string allRows = r.ReadToEnd();
+                            string[] rows = allRows.Split('\n');
+
+                            bool first = true;
+                            foreach (string row in rows)
+                            {
+                                string rowTrim = row;
+                                if (row.IndexOf('\r') > -1)
+                                    rowTrim = row.Remove(row.IndexOf('\r'), 1);
+
+                                if (first)  
+                                {
+                                    // First row is the header row - just copy it
+                                    //output += rowTrim + "\n";
+
+                                    // HACK! (HNV 2020.08.17): The header must look exactly like this (from an older CSV file...)
+                                    output += "Activity;Activity Short description;Username;SSN;Fullname;MobilePhone;Activity start Date;" + 
+                                      "Activity End Date;Initial password;Given name;Surname;Address;Secondary address;Postal code;City;" +
+                                      "IDFromAdministrativeSystem;PrivateMobilePhone;Email;COAddress\n";
+
+                                    first = false;
+                                }
+                                else
+                                {
+                                    // The only fields needed to create the DB's are: class name (repeated 2 times) and student number (field 3).
+                                    // The name of the student (field 5) is nice to have in the file, but not used for the DB
+                                    string[] items = rowTrim.Split(';');
+                                    if (items.Length > 5)
+                                    {
+                                        int count = 0;
+                                        while (count < 5)
+                                        {
+                                            output += items[count];
+                                            if (count < 4)
+                                            {
+                                                output += ";";
+                                            }
+                                            else
+                                            {
+                                                output += tail;
+                                            }
+                                            count++;
+                                        }
+                                    }
+                                }
+                            }
+                        }
+                    }
+
+                    using (FileStream fs = new FileStream(filePathTrimmed, FileMode.Append))
+                    {
+                        using (StreamWriter w = new StreamWriter(fs))
+                        {
+                            w.Write(output);
+                        }
+                    }
+                }
+            }
         }
     }
 }
